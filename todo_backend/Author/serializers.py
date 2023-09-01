d3907b841020bad2a9427bbcd0a70c41403cdc78
Nasser <nasser-silva@hotmail.com>
2023-09-01 02:16:34 -0300

    Remove old email validation

--- todo_backend/Author/serializers.py
+++ todo_backend/Author/serializers.py
@@ -3,19 +3,12 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+
     # TODO: password confirmation
     class Meta:
         model = User
         fields = ["username", "password", "email"]
-
-    def is_email_valid(self, email):
-        return email != None and email != ''
-
-    def validate(self, data):
-        email = data.get('email', None)
-        if not self.is_email_valid(email):
-            raise serializers.ValidationError({"email": "Email must be valid."})
-        return data
 
     def create(self, validated_data):
         user = User.objects.create(**validated_data)
