baaef631897cf3d800042c86e80a1ccfaae52361
Nasser <nasser-silva@hotmail.com>
2023-09-02 01:23:31 -0300

    Add toast

---
+++ todo_frontend/src/components/Toast.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+import styles from './Toast.module.css';
+
+interface IToast {
+    text: string;
+    error?: boolean;
+}
+
+function Toast({ text, error = false }: IToast) {
+    const [visible, setVisible] = useState<boolean>(true);
+    const [timeoutID, setTimeoutID] = useState<NodeJS.Timeout>();
+
+    const close = () => {
+        setVisible(false);
+    }
+
+    const stopTimer = () => {
+        clearTimeout(timeoutID);
+    }
+
+    const startTimer = () => {
+        const timeout = setTimeout(() => setVisible(false), 5000);
+        setTimeoutID(timeout);
+        return timeout;
+    }
+
+    const getStyle = () => {
+        const display = !visible ? { display: "none" } : {};
+
+        return {
+            ...display,
+        }
+    }
+
+    useEffect(() => {
+        const timeout = startTimer();
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, [])
+
+    // TODO: add stacking
+    // TODO: add timer visualization
+
+    return (
+        <div
+            className={styles.container}
+            style={getStyle()}
+            onMouseEnter={() => stopTimer()}
+            onMouseLeave={() => startTimer()}
+        >
+            <div className={styles.close}>
+                <div className={styles.closeIcon} onClick={() => close()}>
+                    x
+                </div>
+            </div>
+            <div className={styles.textContainer}>
+                {text}
+            </div>
+        </div>
+    );
+}
+
+export default Toast;
